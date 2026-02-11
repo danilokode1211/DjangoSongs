f9a68351882e16d1c3c57bbb6abd31b4d910723c
ITDaniel12 <daniel.cardona@trustcore-services.com>
2026-02-11 16:55:05 -0500

    Creacion inicial del modelo canciones

--- canciones/models.py
+++ canciones/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Cancion(models.Model):
+
+    titulo = models.CharField(max_length=100, verbose_name='Titulo')
+    artista = models.CharField(max_length=100, verbose_name='Artista')
+    
+    popularidad = models.IntegerField(
+    
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        verbose_name='Popularidad (1,10)'
+    )
+
+    class Meta:
+
+        db_table = 'cancion' #Nombre pintado en mysql       
+        verbose_name ="Cancion"
+        verbose_name_plural = 'Canciones'
+
+    def __str__(self):
+        return f"{self.titulo} - {self.artista}"  
